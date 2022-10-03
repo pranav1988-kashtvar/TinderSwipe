4cd0de1b043876a864f60d46ba844da305179005
Pranav Kashtvar <pranavkashtvar88@gmail.com>
2022-10-03 20:13:25 +0530

    Initial Commit

---
+++ TinderSwipe/TinderSwipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinderSwipe
+//
+//  Created by Shi Pra on 03/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
